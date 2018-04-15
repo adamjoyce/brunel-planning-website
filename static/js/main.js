c838d0425a3f92a72134ac7bce471470ee445f07
Adam Joyce <adamjoyce@outlook.com>
2018-04-15 17:24:57 +0100

    Mobile nav bar implemented

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,24 @@
 window.onload = function() {
+  // Bind navigation hamburger events.
+  var hamburgers = document.getElementsByClassName('hamburger');
+  var navMenu = document.getElementById('nav-mobile');
+  if (hamburgers && navMenu) {
+    // Toggle the drop-down menu.
+    hamburgers[0].addEventListener('click', function() {
+      navMenu.classList.toggle('opened');
+      // Show the top border if opening.
+      if (navMenu.classList.contains('opened'))
+        navMenu.style.borderTop = '1px solid #fff';
+    });
+    // Remove the top border once closed.
+    navMenu.addEventListener('transitionend', function() {
+      if (!navMenu.classList.contains('opened')) {
+        navMenu.style.borderTop = '0';
+      }
+    });
+  }
+
+  // Side Slide Elements.
   var slideInElements = document.getElementsByClassName('content-wrapper');
   if (slideInElements) {
     for (var i = 0; i < slideInElements.length; i++) {
